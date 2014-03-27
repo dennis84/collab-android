a15747bc86c03d8c22896c95fc0d29a9008e283f
dennis84 <d.dietrich84@gmail.com>
2014-03-27 22:06:44 +0100

    removed test actor

--- src/collab/MainActivity.scala
+++ src/collab/MainActivity.scala
@@ -1,8 +1,6 @@
 package collab.android
 
 import org.scaloid.common._
-import android.content.Context
-import akka.actor._
 
 class MainActivity extends SActivity {
 
@@ -11,21 +9,8 @@ class MainActivity extends SActivity {
       val room = SEditText()
       SButton("Connect", join(room.getText.toString))
     } padding 20.dip
-
-    val system = getApplication.asInstanceOf[CollabApplication].unsafeSystem
-    val actor = system.actorOf(Props(new TestActor(getApplicationContext)))
-
-    actor ! "Hello World"
   }
 
   def join(id: String) =
     startActivity(SIntent[EditorActivity].putExtra("room", id))
-}
-
-class TestActor(c: Context) extends Actor {
-  implicit val ctx = c
-
-  def receive = {
-    case x: String ⇒ toast(x)
-  }
 }
