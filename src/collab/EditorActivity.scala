f7ec7a0ec97481dfba9c492043889697e9bf3727
dennis84 <d.dietrich84@gmail.com>
2014-04-04 20:34:49 +0200

    updated colors, added line numbers

--- src/collab/EditorActivity.scala
+++ src/collab/EditorActivity.scala
@@ -13,8 +13,10 @@ class EditorActivity extends SActivity {
                   |""".stripMargin
 
     val html = Colors(code, "scala") {
-      case WordCode(v)       ⇒ s"""<font color="blue">$v</font>"""
-      case TextCode(v)       ⇒ s"""<font color="green">$v</font>"""
+      case WordCode(v)       ⇒ s"""<font color="${Theme.Violet}">$v</font>"""
+      case TextCode(v)       ⇒ s"""<font color="${Theme.Green}">$v</font>"""
+      case CommentCode(v)    ⇒ s"""<font color="${Theme.Base1}">$v</font>"""
+      case BracketCode(v)    ⇒ s"""<font color="${Theme.Red}">$v</font>"""
       case WhitespaceCode(v) ⇒ v replace (" ", "&nbsp;")
       case c: Code           ⇒ c.value
     } replace ("\n", "<br>")
@@ -22,8 +24,16 @@ class EditorActivity extends SActivity {
     val spannedCode = Html.fromHtml(
       """<font face="monospace">""" + html + "</font>")
 
-    contentView = new SVerticalLayout {
-      STextView(spannedCode)
+    val lineNumbersHtml = (1 to (code split "\n").length) map { n ⇒
+      s"""<font color="${Theme.Base1}">$n</font>"""
+    } mkString("<br>")
+
+    val spannedLineNumbers = Html.fromHtml(
+      """<font face="monospace">""" + lineNumbersHtml + "</font>")
+
+    contentView = new SRelativeLayout {
+      val ln = STextView(spannedLineNumbers)
+      STextView(spannedCode).<<.rightOf(ln).>>.marginLeft(25 sp)
     }
   }
 }
---
+++ src/collab/Theme.scala
@@ -0,0 +1,25 @@
+package collab.android
+
+object Theme {
+
+  // background tones (dark)
+  val Base03  = "#2B2F3E"
+  val Base02  = "#3E4457"
+  // content tones
+  val Base01  = "#4B4E67"
+  val Base00  = "#646889"
+  val Base0   = "#767b9e"
+  val Base1   = "#CDD1DD"
+  // background tones (light)
+  val Base2   = "#fefefe"
+  val Base3   = "#fff"
+
+  val Yellow  = "#D7FF98"
+  val Orange  = "#FAA486"
+  val Red     = "#ED7E79"
+  val Magenta = "#ff5f97"
+  val Violet  = "#948cf1"
+  val Blue    = "#7376F0"
+  val Cyan    = "#2fe7be"
+  val Green   = "#32b95f"
+}
