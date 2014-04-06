9f5977f0aa1de773a81845edc31be2d91a541fdc
dennis84 <d.dietrich84@gmail.com>
2014-04-06 18:08:07 +0200

    removed ActorSystem

--- src/collab/CollabApplication.scala
+++ src/collab/CollabApplication.scala
@@ -1,21 +1,14 @@
 package collab.android
 
 import android.app.Application
-import akka.actor._
 
 class CollabApplication extends Application {
 
-  var system: Option[ActorSystem] = None
-
-  def unsafeSystem = system.get
-
   override def onCreate() {
     super.onCreate
-    system = Some(ActorSystem("collab"))
   }
 
   override def onTerminate() {
     super.onTerminate
-    system foreach (_.shutdown)
   }
 }
