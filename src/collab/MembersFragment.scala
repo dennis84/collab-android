1a8ef1234d964293f541da702bb90013ebbf27e2
dennis84 <d.dietrich84@gmail.com>
2014-04-13 16:17:34 +0200

    save members state

--- src/collab/MembersFragment.scala
+++ src/collab/MembersFragment.scala
@@ -9,19 +9,28 @@ import org.scaloid.common._
 
 class MembersFragment extends ListFragment {
 
+  var members = new ArrayList[String]()
   var adapter: ArrayAdapter[String] = _
 
   override def onActivityCreated(savedInstanceState: Bundle) {
     super.onActivityCreated(savedInstanceState)
     implicit val ctx = getActivity
 
-    val members = new ArrayList[String]()
     val headline = new STextView("Who's Online")
     headline.padding(16 dip)
     getListView.addHeaderView(headline)
 
+    if(null != savedInstanceState) {
+      members = savedInstanceState getStringArrayList "members"
+    }
+
     adapter = new ArrayAdapter[String](getActivity, R.layout.member, members)
     setListAdapter(adapter)
+  }
+
+  override def onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    outState.putStringArrayList("members", members)
   }
 
   def update(member: String) {
