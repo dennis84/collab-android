6e20a8842ef0876b97554c89409e4e08ff2099c9
dennis84 <d.dietrich84@gmail.com>
2014-04-13 16:13:41 +0200

    added editor and members fragments

---
+++ src/collab/EditorFragment.scala
@@ -0,0 +1,70 @@
+package collab.android
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.{LayoutInflater, ViewGroup}
+import android.widget.TextView
+import android.graphics.Typeface
+import android.text.Html
+import spray.json._
+import colors._
+
+class EditorFragment extends Fragment {
+  import MessageProtocol._
+
+  var code: TextView = _
+  var lineNumbers: TextView = _
+
+  override def onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup,
+    savedInstanceState: Bundle
+  ) = inflater.inflate(R.layout.editor, container, false)
+
+  override def onActivityCreated(savedInstanceState: Bundle) {
+    super.onActivityCreated(savedInstanceState)
+
+    code        = getActivity.findViewById(R.id.code).asInstanceOf[TextView]
+    lineNumbers = getActivity.findViewById(R.id.lineNumbers).asInstanceOf[TextView]
+
+    val font = Typeface.createFromAsset(getActivity.getAssets,
+      "FantasqueSansMono-Regular.ttf")
+
+    code.setTypeface(font)
+    lineNumbers.setTypeface(font)
+
+    if(null != savedInstanceState) {
+      code setText savedInstanceState.getCharSequence("buffer")
+      lineNumbers setText savedInstanceState.getCharSequence("lineNumbers")
+    }
+  }
+
+  override def onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    outState.putCharSequence("buffer", code getText)
+    outState.putCharSequence("lineNumbers", lineNumbers getText)
+  }
+
+  def update(message: String) {
+    val m = message.asJson.convertTo[CodeMessage]
+    val html = Colors(m.buffer, m.lang) {
+      case WordCode(v)       ⇒ s"""<font color="${Theme.Violet}">$v</font>"""
+      case TextCode(v)       ⇒ s"""<font color="${Theme.Green}">$v</font>"""
+      case CommentCode(v)    ⇒ s"""<font color="${Theme.Base1}">$v</font>"""
+      case BracketCode(v)    ⇒ s"""<font color="${Theme.Red}">$v</font>"""
+      case WhitespaceCode(v) ⇒ v replace (" ", "&nbsp;")
+      case c: Code           ⇒ c.value
+    } replace ("\n", "<br>")
+
+    val spannedCode = Html.fromHtml(html)
+
+    val lineNumbersHtml = (1 to (m.buffer split "\n").length) map { n ⇒
+      s"""<font color="${Theme.Base1}">$n&nbsp;</font>"""
+    } mkString("<br>")
+
+    val spannedLineNumbers = Html.fromHtml(lineNumbersHtml)
+
+    code setText spannedCode
+    lineNumbers setText spannedLineNumbers
+  }
+}
--- src/collab/MainActivity.scala
+++ src/collab/MainActivity.scala
@@ -1,48 +1,23 @@
 package collab.android
 
 import org.scaloid.common._
-import android.view.View
-import android.content._
 
 class MainActivity extends SActivity {
 
   onCreate {
-    val room = new SEditText
-    val joinButton = new SButton("Connect", join(room.text.toString))
-    val leaveButton = new SButton("Leave", leave)
-    val joinLayout = new SVerticalLayout
-    val leaveLayout = new SVerticalLayout
-
-    contentView = new SVerticalLayout {
-      joinLayout += room
-      joinLayout += joinButton
-      joinLayout padding 20.dip
-
-      leaveLayout += leaveButton
-      leaveLayout padding 20.dip
-      leaveLayout visibility View.GONE
-
-      this += joinLayout
-      this += leaveLayout
-    }
-
-    broadcastReceiver(Connection.Join) { (context, intent) ⇒
-      startActivity(SIntent[EditorActivity])
-      joinLayout.visibility(View.GONE)
-      leaveLayout.visibility(View.VISIBLE)
-    }
-
-    broadcastReceiver(Connection.Leaved) { (context, intent) ⇒
-      joinLayout.visibility(View.VISIBLE)
-      leaveLayout.visibility(View.GONE)
-    }
-
-    startService(SIntent[ConnectionService])
+    setContentView(R.layout.main)
   }
 
-  def join(id: String) =
-    sendBroadcast(new Intent(Connection.Join).putExtra("uri",
-      getString(R.string.endpoint) + "/" + id))
+  broadcastReceiver(Message.Join) { (context, intent) ⇒
+    fragment[MembersFragment](R.id.members).update(
+      intent.getStringExtra("data"))
+  }
 
-  def leave = sendBroadcast(new Intent(Connection.Leave))
+  broadcastReceiver(Message.Code) { (context, intent) ⇒
+    fragment[EditorFragment](R.id.editor).update(
+      intent.getStringExtra("data"))
+  }
+
+  def fragment[T](res: Int) =
+    getFragmentManager.findFragmentById(res).asInstanceOf[T]
 }
